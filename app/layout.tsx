239b229006021da2c30974d1ee1be2269f6b0e0f
Matthew Ryan Granger <mattg1827@gmail.com>
2025-02-28 01:19:02 -0800

    Website beginnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Lato } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lato = Lato({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: "100"
 });
 
 export const metadata: Metadata = {
@@ -23,10 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className="bg-stone-950 text-slate-100">
+      <body className={`${lato.className}>`}>
         {children}
       </body>
     </html>
