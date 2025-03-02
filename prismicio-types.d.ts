bb30485cd5c37d225efce63e961b2e418af5128a
Matthew Ryan Granger <mattg1827@gmail.com>
2025-03-02 01:32:37 -0800

    New Tech List in Bio made

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -69,7 +69,7 @@ export type HomepageDocument<Lang extends string = string> =
     Lang
   >;
 
-type PageDocumentDataSlicesSlice = BiographySlice;
+type PageDocumentDataSlicesSlice = TechListSlice | BiographySlice;
 
 /**
  * Content for Page documents
@@ -321,32 +321,6 @@ export interface BiographySliceDefaultPrimary {
   biography_paragraph: prismic.RichTextField;
 
   /**
-   * Button Text field in *Biography → Default → Primary*
-   *
-   * - **Field Type**: Text
-   * - **Placeholder**: *None*
-   * - **API ID Path**: biography.default.primary.button_text
-   * - **Documentation**: https://prismic.io/docs/field#key-text
-   */
-  button_text: prismic.KeyTextField;
-
-  /**
-   * Button Link field in *Biography → Default → Primary*
-   *
-   * - **Field Type**: Link
-   * - **Placeholder**: *None*
-   * - **API ID Path**: biography.default.primary.button_link
-   * - **Documentation**: https://prismic.io/docs/field#link-content-relationship
-   */
-  button_link: prismic.LinkField<
-    string,
-    string,
-    unknown,
-    prismic.FieldState,
-    never
-  >;
-
-  /**
    * Avatar field in *Biography → Default → Primary*
    *
    * - **Field Type**: Image
@@ -459,6 +433,88 @@ type HeroSliceVariation = HeroSliceDefault;
  */
 export type HeroSlice = prismic.SharedSlice<"hero", HeroSliceVariation>;
 
+/**
+ * Item in *TechList → Default → Primary → Tech List Group*
+ */
+export interface TechListSliceDefaultPrimaryTechListGroupItem {
+  /**
+   * Tech Name field in *TechList → Default → Primary → Tech List Group*
+   *
+   * - **Field Type**: Text
+   * - **Placeholder**: *None*
+   * - **API ID Path**: tech_list.default.primary.tech_list_group[].tech_name
+   * - **Documentation**: https://prismic.io/docs/field#key-text
+   */
+  tech_name: prismic.KeyTextField;
+
+  /**
+   * Tech Color field in *TechList → Default → Primary → Tech List Group*
+   *
+   * - **Field Type**: Color
+   * - **Placeholder**: *None*
+   * - **API ID Path**: tech_list.default.primary.tech_list_group[].tech_color
+   * - **Documentation**: https://prismic.io/docs/field#color
+   */
+  tech_color: prismic.ColorField;
+}
+
+/**
+ * Primary content in *TechList → Default → Primary*
+ */
+export interface TechListSliceDefaultPrimary {
+  /**
+   * Heading field in *TechList → Default → Primary*
+   *
+   * - **Field Type**: Text
+   * - **Placeholder**: *None*
+   * - **API ID Path**: tech_list.default.primary.heading
+   * - **Documentation**: https://prismic.io/docs/field#key-text
+   */
+  heading: prismic.KeyTextField;
+
+  /**
+   * Tech List Group field in *TechList → Default → Primary*
+   *
+   * - **Field Type**: Group
+   * - **Placeholder**: *None*
+   * - **API ID Path**: tech_list.default.primary.tech_list_group[]
+   * - **Documentation**: https://prismic.io/docs/field#group
+   */
+  tech_list_group: prismic.GroupField<
+    Simplify<TechListSliceDefaultPrimaryTechListGroupItem>
+  >;
+}
+
+/**
+ * Default variation for TechList Slice
+ *
+ * - **API ID**: `default`
+ * - **Description**: Default
+ * - **Documentation**: https://prismic.io/docs/slice
+ */
+export type TechListSliceDefault = prismic.SharedSliceVariation<
+  "default",
+  Simplify<TechListSliceDefaultPrimary>,
+  never
+>;
+
+/**
+ * Slice variation for *TechList*
+ */
+type TechListSliceVariation = TechListSliceDefault;
+
+/**
+ * TechList Shared Slice
+ *
+ * - **API ID**: `tech_list`
+ * - **Description**: TechList
+ * - **Documentation**: https://prismic.io/docs/slice
+ */
+export type TechListSlice = prismic.SharedSlice<
+  "tech_list",
+  TechListSliceVariation
+>;
+
 declare module "@prismicio/client" {
   interface CreateClient {
     (
@@ -498,6 +554,11 @@ declare module "@prismicio/client" {
       HeroSliceDefaultPrimary,
       HeroSliceVariation,
       HeroSliceDefault,
+      TechListSlice,
+      TechListSliceDefaultPrimaryTechListGroupItem,
+      TechListSliceDefaultPrimary,
+      TechListSliceVariation,
+      TechListSliceDefault,
     };
   }
 }
---
+++ slices/TechList/index.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from "react";
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+import Heading from "@/components/Heading";
+
+/**
+ * Props for `TechList`.
+ */
+export type TechListProps = SliceComponentProps<Content.TechListSlice>;
+
+/**
+ * Component for "TechList" Slices.
+ */
+const TechList: FC<TechListProps> = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <Heading size="lg" as="h2" className="text-slate-700">
+        {slice.primary.heading}
+      </Heading>
+      {slice.primary.tech_list_group.map(({ tech_color, tech_name }, index) => (
+        <div key={index}>
+          {Array.from({ length: 15 }, (_, index) => (
+            <React.Fragment key={index}>
+              <span className="tech-item">{tech_name}</span>
+            </React.Fragment>
+          ))}
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default TechList;
